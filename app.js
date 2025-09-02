e0b5b63dd53b90dc1944697035cf3dd1f82446cf
Rohan-Saxena644 <rohsax83@gmail.com>
2025-09-02 19:45:38 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
+
+main()
+    .then(()=>{
+        console.log("Connected to db") ;
+    }).catch((err)=>{
+        console.log(err) ;
+    }) ;
+
+async function main(){
+    await mongoose.connect(MONGO_URL) ;
+}
+
+app.set("view engine","ejs") ;
+app.set("views", path.join(__dirname,"views")) ;
+app.use(express.urlencoded({extended: true})) ;
+
+app.get("/",(req,res)=>{
+    res.send("Hello,I am root") ;
+})
+
+//Index Route
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({}) ;
+    res.render("listings/index",{allListings}) ;
+})
+
+//show route
+app.get("/listings/:id", async (req,res)=>{
+    let {_id} = req.params ;
+    const listing = await Listing.findById(_id) ;
+    res.render("listings/show",{listing}) ;
+})
+
+
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title: "My new Villa",
+//         description: "By the Beach",
+//         price: 1200,
+//         location: "Calangute,Goa",
+//         country: "India",
+//     })
+
+//     await sampleListing.save() ;
+//     console.log("Sample was saved") ;
+//     res.send("Successful Testing");
+// }) ;
+
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080") ;
+})
+
---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose") ;
+const Schema = mongoose.Schema ;
+
+const listingSchema = new Schema({
+    title: {
+        type: String ,
+        required: true ,
+    },
+    description: String,
+    image:{
+        filename: String ,
+        url: String,
+    //     // default:
+    //     //     "https://i.pinimg.com/736x/b8/6b/17/b86b170e78a07c37e8001350c92123bb.jpg",
+    //     // set: (v)=>
+    //     //     v==="" 
+    //     //     ? "https://i.pinimg.com/736x/b8/6b/17/b86b170e78a07c37e8001350c92123bb.jpg"
+    //     //     : v ,
+    },
+    
+    price: Number,
+    location: String,
+    country: String,
+}) ;
+
+const Listing = mongoose.model("Listing",listingSchema) ;
+module.exports = Listing ;
