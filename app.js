5b962831e8df943b0e3a68bab25f25a88e556d98
Rohan-Saxena644 <rohsax83@gmail.com>
2025-09-05 17:35:03 +0530

    Added the update route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
 
@@ -20,6 +21,7 @@ async function main(){
 app.set("view engine","ejs") ;
 app.set("views", path.join(__dirname,"views")) ;
 app.use(express.urlencoded({extended: true})) ;
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("Hello,I am root") ;
@@ -60,6 +62,23 @@ app.get("/listings/:id/edit",async(req,res)=>{
     const listing = await Listing.findById(id) ;
     res.render("listings/edit.ejs",{listing}) ;
 })
+
+
+//UPDATE ROUTE
+app.put("/listings/:id",async(req,res)=>{
+    let{id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing}); //HUMNE de construct kiy hai listing
+    res.redirect(`/listings/${id}`);
+})                                                       //object ko jo humne name=listing[id]
+                                                         //listing[description] naam rakhe the uski wajah se
+
+
+
+// DELETE ROUTE
+                                                         
+
+
+
 
 
 
