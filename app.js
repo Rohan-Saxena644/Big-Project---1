0f59409e97e6a1f9e48672b263cf3dad0ef86a03
Rohan-Saxena644 <rohsax83@gmail.com>
2025-09-22 23:43:02 +0530

    Fixing errors in the project part1

--- app.js
+++ app.js
@@ -103,7 +103,8 @@ app.all("*",(req,res,next)=>{
 //Error handling middleware
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something went wrong"} = err;
-    res.status(statusCode).send(message);
+    res.render("error.ejs",{message});
+    // res.status(statusCode).send(message);
 });
 
 
