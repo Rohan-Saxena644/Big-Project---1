df9a80d2c82111e0b549f259cf7bbd1387dbdd1f
Rohan-Saxena644 <rohsax83@gmail.com>
2025-09-23 23:19:18 +0530

    Finished error.ejs

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.all("*",(req,res,next)=>{
 //Error handling middleware
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something went wrong"} = err;
-    res.render("error.ejs",{message});
+    res.status(statusCode).render("error.ejs",{message});
     // res.status(statusCode).send(message);
 });
 
