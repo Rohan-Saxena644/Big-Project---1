d6a905d133b14cd82cfc3b703577b310313a1f5f
Rohan-Saxena644 <rohsax83@gmail.com>
2025-09-25 22:29:26 +0530

    made app.js code clean

--- app.js
+++ app.js
@@ -64,33 +64,7 @@ app.get("/listings/:id", async (req,res)=>{
 
 //Create route
 app.post("/listings",validateListing, wrapAsync (async (req,res,next)=>{
-    // let {title,description,image,price,country,location} = req.body;
-    // let listing = req.body.listing;
-    // console.log(listing);
-        // if(!req.body.listing){
-        //     throw new ExpressError(400,"Send valid data for listing");
-        // }
-        
-        //SCHEMA VALIDATIONS
-        // let result = listingSchema.validate(req.body);
-        // console.log(result);
-
-        // if(result.error){
-        //     throw new ExpressError(400,result.error);
-        // }
-
         const newListing = new Listing(req.body.listing);
-        // if(!newListing.title){
-        //     throw new ExpressError(400,"Title is missing");
-        // }
-
-        // if(!newListing.description){
-        //     throw new ExpressError(400,"Description is missing");
-        // }
-
-        // if(!newListing.location){
-        //     throw new ExpressError(400,"Location is missing");
-        // }
         await newListing.save();
         res.redirect("/listings");
 } ));
@@ -105,15 +79,10 @@ app.get("/listings/:id/edit",async(req,res)=>{
 
 //UPDATE ROUTE
 app.put("/listings/:id", validateListing, async(req,res)=>{
-    // commenting because of validateListing function
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send valid data for listing");
-    // }
     let{id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing}); //HUMNE de construct kiy hai listing
+    await Listing.findByIdAndUpdate(id,{...req.body.listing}); 
     res.redirect(`/listings/${id}`);
-});                                                       //object ko jo humne name=listing[id]
-                                                         //listing[description] naam rakhe the uski wajah se
+});                                                     
 
 
 
@@ -126,36 +95,17 @@ app.delete("/listings/:id", async (req,res)=>{
 
 
 
-
-
-
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not found"));
 })
+
 
 //Error handling middleware
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something went wrong!"} = err;
     res.status(statusCode).render("error.ejs",{message});
-    // res.status(statusCode).send(message);
 });
 
-
-
-
-// app.get("/testListing",async (req,res)=>{
-//     let sampleListing = new Listing({
-//         title: "My new Villa",
-//         description: "By the Beach",
-//         price: 1200,
-//         location: "Calangute,Goa",
-//         country: "India",
-//     })
-
-//     await sampleListing.save() ;
-//     console.log("Sample was saved") ;
-//     res.send("Successful Testing");
-// }) ;
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080") ;
