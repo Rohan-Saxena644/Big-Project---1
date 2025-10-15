4d84915fd679dd34bd4a18d35e12851a9c8e94b8
Rohan-Saxena644 <rohsax83@gmail.com>
2025-10-15 22:07:10 +0530

    Finished error handling for the reviews

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema , reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust" ;
@@ -37,6 +37,17 @@ app.get("/",(req,res)=>{
 // middleware for schema validations
 const validateListing = (req,res,next) =>{
     let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+// middleware to validate reviews
+const validateReview = (req,res,next) =>{
+    let {error} = reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400,errMsg);
@@ -97,7 +108,7 @@ app.delete("/listings/:id", async (req,res)=>{
 
 // Reviews
 // Post Route
-app.post("/listings/:id/reviews" , async (req,res) =>{
+app.post("/listings/:id/reviews" , validateReview , wrapAsync( async (req,res) =>{
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -110,7 +121,7 @@ app.post("/listings/:id/reviews" , async (req,res) =>{
     // res.send("new review saved");
 
     res.redirect(`/listings/${listing._id}`);
-})
+}));
 
 
 
