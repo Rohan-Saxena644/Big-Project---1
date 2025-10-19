4480d2d85aa49ff2d1ddfec1f2bce7aaa879a1f6
Rohan-Saxena644 <rohsax83@gmail.com>
2025-10-19 22:55:51 +0530

    Saw session id related stuff and practiced it in demo classroom folder

--- classroom/server.js
+++ classroom/server.js
@@ -2,38 +2,46 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
+// const cookieParser = require("cookie-parser");
 
-app.use(cookieParser("secretcode"));
+// app.use(cookieParser("secretcode"));
 
-app.get("/signedcookie" , (req,res)=>{
-    res.cookie("made-in" , "India" , {signed : true});
-    res.send("signed cookie sent");
+// app.get("/signedcookie" , (req,res)=>{
+//     res.cookie("made-in" , "India" , {signed : true});
+//     res.send("signed cookie sent");
+// });
+
+// app.get("/verify" , (req,res)=>{
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// }); // if the output on console is {} {key: false} then the signed cookie has been tempered with
+
+// app.get("/getcookies",(req,res)=>{
+//     res.cookie("great" , "hello");
+//     res.cookie("madeIn" , "India");
+//     res.send("sent you some cookies!");
+// });
+
+// app.get("/greet" , (req,res)=>{
+//     let {name = "anonymous"} = req.cookies ;
+//     res.send(`Hi, ${name}`);
+// })
+
+// app.get("/" , (req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("Hi i am root");
+// });
+
+// app.use("/users",users);
+// app.use("/posts",posts);
+
+
+app.use(session({secret: "mysupersecretstring"}));
+
+app.get("/test" , (req,res)=>{
+    res.send("test successful!");
 });
-
-app.get("/verify" , (req,res)=>{
-    console.log(req.signedCookies);
-    res.send("verified");
-}); // if the output on console is {} {key: false} then the signed cookie has been tempered with
-
-app.get("/getcookies",(req,res)=>{
-    res.cookie("great" , "hello");
-    res.cookie("madeIn" , "India");
-    res.send("sent you some cookies!");
-});
-
-app.get("/greet" , (req,res)=>{
-    let {name = "anonymous"} = req.cookies ;
-    res.send(`Hi, ${name}`);
-})
-
-app.get("/" , (req,res)=>{
-    console.dir(req.cookies);
-    res.send("Hi i am root");
-});
-
-app.use("/users",users);
-app.use("/posts",posts);
 
 app.listen(3000,()=>{
     console.log("server is listening to 3000");
