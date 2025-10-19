cf2465c1f25ad46ef0ed0e9efb4cc2122e67574d
Rohan-Saxena644 <rohsax83@gmail.com>
2025-10-19 17:02:52 +0530

    Added cookie parser to the project

--- classroom/server.js
+++ classroom/server.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
 
 app.get("/getcookies",(req,res)=>{
     res.cookie("great" , "hello");
     res.cookie("madeIn" , "India");
     res.send("sent you some cookies!");
 });
+
+app.get("/greet" , (req,res)=>{
+    let {name = "anonymous"} = req.cookies ;
+    res.send(`Hi, ${name}`);
+})
 
 app.get("/" , (req,res)=>{
     console.dir(req.cookies);
