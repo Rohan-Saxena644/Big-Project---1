865e005d44757074cc8b1fa841469cab90bb24c0
Rohan-Saxena644 <rohsax83@gmail.com>
2025-10-20 14:38:58 +0530

    using connect-flash package

--- classroom/server.js
+++ classroom/server.js
@@ -4,7 +4,11 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const path = require("path");
 // const cookieParser = require("cookie-parser");
+
+app.set("view engine","ejs") ;
+app.set("views", path.join(__dirname,"views")) ;
 
 // app.use(cookieParser("secretcode"));
 
@@ -50,11 +54,13 @@ app.use(flash());
 app.get("/register" , (req,res)=>{
     let {name = "anonymous"} = req.query ;
     req.session.name = name ;
+    req.flash("success","user registered successfully!");
     res.redirect("/hello");
 });
 
 app.get("/hello",(req,res)=>{
-    res.send(`hello, ${req.session.name}`);  // ek single session chal raha hai hence we can use this data everywhere form one route to other route
+    res.render("page.ejs" , {name: req.session.name , msg: req.flash("success")});
+    // res.send(`hello, ${req.session.name}`);  // ek single session chal raha hai hence we can use this data everywhere form one route to other route
 });
 
 // app.get("/reqcount" , (req,res)=>{
