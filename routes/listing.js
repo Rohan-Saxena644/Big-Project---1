071f6f97e7697374fbaa12704c738f4efac224fc
Rohan-Saxena644 <rohsax83@gmail.com>
2025-10-21 15:33:21 +0530

    Added flash failure includes

--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,10 @@ router.get("/new" , async (req,res)=>{
 router.get("/:id", async (req,res)=>{
     let {id} = req.params ;
     const listing = await Listing.findById(id).populate("reviews") ;
+    if(!listing){
+        req.flash("error","Lisitng you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/show",{listing}) ;
 });
 
@@ -46,6 +50,10 @@ router.post("/",validateListing, wrapAsync (async (req,res,next)=>{
 router.get("/:id/edit",async(req,res)=>{
     let{id} = req.params;
     const listing = await Listing.findById(id) ;
+    if(!listing){
+        req.flash("error","Lisitng you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs",{listing}) ;
 });
 
