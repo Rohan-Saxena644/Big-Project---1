4cfac5bb42f06ac9e2b0a35815dc88dced5f1294
Rohan-Saxena644 <rohsax83@gmail.com>
2025-11-06 12:27:49 +0530

    Added Router.route to make our routing even more compact

--- routes/listing.js
+++ routes/listing.js
@@ -5,28 +5,49 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn , isOwner , validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
-//Index Route
-router.get("/", wrapAsync(listingController.index));
+
+router
+    .route("/")
+    .get( wrapAsync(listingController.index))
+    .post(isLoggedIn ,validateListing, wrapAsync(listingController.createListing));
+
 
 //New Route
-router.get("/new" , isLoggedIn , wrapAsync(listingController.renderNewForm));
+router.get("/new" , isLoggedIn , wrapAsync(listingController.renderNewForm));//It is defined here as the router.route can interpret our new as a id    
 
-//show route
-router.get("/:id", wrapAsync(listingController.showListing));
 
-//Create route
-router.post("/", isLoggedIn ,validateListing, wrapAsync(listingController.createListing));
+router.route("/id")
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn , isOwner , validateListing , wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn , isOwner , wrapAsync(listingController.destroyListing));
 
+
+    
 //Edit route
 router.get("/:id/edit", isLoggedIn , isOwner , wrapAsync(listingController.renderEditForm));
 
 
-//UPDATE ROUTE
-router.put("/:id", isLoggedIn , isOwner , validateListing , wrapAsync(listingController.updateListing));                                                     
+
+// //Index Route
+// router.get("/", wrapAsync(listingController.index));
+
+
+// //show route
+// router.get("/:id", wrapAsync(listingController.showListing));
+
+// //Create route
+// router.post("/", isLoggedIn ,validateListing, wrapAsync(listingController.createListing));
+
+// //Edit route
+// router.get("/:id/edit", isLoggedIn , isOwner , wrapAsync(listingController.renderEditForm));
+
+
+// //UPDATE ROUTE
+// router.put("/:id", isLoggedIn , isOwner , validateListing , wrapAsync(listingController.updateListing));                                                     
 
 
 
-// DELETE ROUTE
-router.delete("/:id", isLoggedIn , isOwner , wrapAsync(listingController.destroyListing));
+// // DELETE ROUTE
+// router.delete("/:id", isLoggedIn , isOwner , wrapAsync(listingController.destroyListing));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,14 +6,20 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/user.js");
 
-router.get("/signup" , userController.renderSignupForm);
 
-router.post("/signup" , wrapAsync(userController.signup));
+router.route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
+// router.get("/signup" , userController.renderSignupForm);
 
-router.get("/login" , userController.renderLoginForm);
+// router.post("/signup" , wrapAsync(userController.signup));
+
+router.route("/login")
+    .get(userController.renderLoginForm)
+    .post(saveRedirectUrl , passport.authenticate("local" , {failureRedirect: "/login" , failureFlash: true}) , userController.login);
 
 //passport.authenticate() is a function which is used as root middleware to authenticate requests
-router.post("/login", saveRedirectUrl , passport.authenticate("local" , {failureRedirect: "/login" , failureFlash: true}) , userController.login);
+// router.post("/login", saveRedirectUrl , passport.authenticate("local" , {failureRedirect: "/login" , failureFlash: true}) , userController.login);
 
 router.get("/logout" , userController.logout);
 
