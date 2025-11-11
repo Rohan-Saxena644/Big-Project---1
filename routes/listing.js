7b05c1d57ce29da4d02adbe28f1a7fb463b8e7cd
Rohan-Saxena644 <rohsax83@gmail.com>
2025-11-11 19:06:12 +0530

    Edit Listing Image

--- routes/listing.js
+++ routes/listing.js
@@ -20,7 +20,7 @@ router.get("/new" , isLoggedIn , wrapAsync(listingController.renderNewForm));//I
 
 router.route("/:id")
     .get(wrapAsync(listingController.showListing))
-    .put(isLoggedIn , isOwner , validateListing , wrapAsync(listingController.updateListing))
+    .put(isLoggedIn , isOwner , upload.single('listing[image]') ,validateListing , wrapAsync(listingController.updateListing))
     .delete(isLoggedIn , isOwner , wrapAsync(listingController.destroyListing));
 
 
