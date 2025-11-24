9e2568d932f76141447a7da0bee9e1544db44a8d
Rohan-Saxena644 <rohsax83@gmail.com>
2025-11-24 16:56:52 +0530

    addded node_modules in git ignore

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ store.on("error",(err)=>{
 
 const sessionOptions = {
     store,
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
